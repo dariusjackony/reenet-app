1dc77a73a347c8447b5bc0b67be971cbbce155eb
Darius Jackony <dariusjackon11@gmail.com>
2026-05-27 23:31:26 +0300

    :: :: :: :: :: :: :: ::

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,11 +2,12 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
+import { Ionicons } from '@expo/vector-icons'
 const profile = () => {
   return (
     <SafeAreaView className='flex-1 bg-black'>
       <StatusBar style='light' />
-      <View className="px-4 mt-4">
+      <View className="px-4 mt-10">
         <View className="flex-row items-center justify-between">
           <Image
             source={require("../../assets/images/pfp.jpeg")}
@@ -15,32 +16,41 @@ const profile = () => {
 
           <View className="flex-row flex-1 justify-around ml-6">
             <View className="items-center">
-              <Text className="text-white font-bold text-base">48</Text>
-              <Text className="text-gray-400 text-xs">Posts</Text>
+              <Text className="text-white text-2xl font-bold text-base">48</Text>
+              <Text className="text-gray-400 text-base">Posts</Text>
             </View>
             <View className="items-center">
-              <Text className="text-white font-bold text-base">100</Text>
-              <Text className="text-gray-400 text-xs">Followers</Text>
+              <Text className="text-white text-2xl font-bold text-base">100</Text>
+              <Text className="text-gray-400 text-base">Followers</Text>
             </View>
             <View className="items-center">
-              <Text className="text-white font-bold text-base">120K</Text>
-              <Text className="text-gray-400 text-xs">Following</Text>
+              <Text className="text-white  text-2xl font-bold text-base">120K</Text>
+              <Text className="text-gray-400 text-base">Following</Text>
             </View>
           </View>
 
         </View>
 
-        <Text className="text-white font-semibold mt-3">Darius Jackony</Text>
-        <Text className="text-gray-300 text-sm mt-0.5 leading-5">
+        <Text className="text-white text-2xl font-semibold mt-3">Darius Jackony</Text>
+        <Text className="text-gray-300 text-base mt-0.5 leading-5">
           I'm a hustler trusting and having a strong faith in God.
         </Text>
-
-        <View className="flex-row gap-2 mt-3">
-          <TouchableOpacity className="flex-1 bg-zinc-800 rounded-lg py-1.5 items-center">
-            <Text className="text-white font-semibold text-sm">Edit profile</Text>
+        <View className='mt-2'>
+          <View className='flex-row gap-2'>
+            <Ionicons name="location-outline" size={16} color="white"/>
+            <Text className='text-gray-400'>Kampala, Uganda</Text>
+          </View>
+          <View className='mt-2 flex-row gap-2'>
+            <Ionicons name='calendar-outline'  size={16} color="white" />
+            <Text className='text-gray-400'>Joined December 2021 </Text>
+          </View>
+        </View>
+        <View className="flex-row gap-2 mt-5">
+          <TouchableOpacity className="flex-1 bg-zinc-800 rounded-lg py-1.5 h-10 items-center">
+            <Text className="text-white font-semibold text-base">Edit profile</Text>
           </TouchableOpacity>
-          <TouchableOpacity className="flex-1 bg-zinc-800 rounded-lg py-1.5 items-center">
-            <Text className="text-white font-semibold text-sm">Share profile</Text>
+          <TouchableOpacity className="flex-1 bg-zinc-800 rounded-lg py-1.5 h-10 items-center">
+            <Ionicons name='settings-outline' size={20} color="white"/>
           </TouchableOpacity>
         </View>
 
